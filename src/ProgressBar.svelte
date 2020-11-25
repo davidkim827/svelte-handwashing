583d339b843e09505c32a91054c2b732d7f537ce
davidkim827 <davidkim827@gmail.com>
2020-11-25 13:37:43 -0500

    make timer and progress bar

--- src/ProgressBar.svelte
+++ src/ProgressBar.svelte
@@ -1,3 +1,7 @@
+<script>
+    export let progress = 0;
+</script>
+
 <style>
 .progress-container {
     margin: 20px 0;
@@ -13,7 +17,7 @@
 <div bp="grid">
     <div bp="offset-5@md 4@md 12@sm"
     class="progress-container">
-    <div class="progress-bar" style="width: 20%;">
+    <div class="progress-bar" style="width: {progress}%;">
         <span class="sr-only">%</span>
     </div>
     </div>
